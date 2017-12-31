07f5f1daaaac36057484b724fa78c811de9a1fde
Jimmy Chen <jzc8@cornell.edu>
2017-12-31 00:02:06 -0500

    Update gitignore for settings file

--- ez-dogspotting/src/App.js
+++ ez-dogspotting/src/App.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 import logo from './logo.svg';
 import Post from './Components/Post'
 import './App.css';
+import * as settings from './settings'
 
 class App extends Component {
 
   componentDidMount() {
+
     axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
       .then(function(response) {
         console.log(response.data);
