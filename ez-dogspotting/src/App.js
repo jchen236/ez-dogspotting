54eacda560073fc2aeddff05240bce7de3e36e92
Jimmy Chen <jzc8@cornell.edu>
2017-12-31 02:37:47 -0500

    Rough template of App.js

--- ez-dogspotting/src/App.js
+++ ez-dogspotting/src/App.js
@@ -4,8 +4,21 @@ import logo from './logo.svg';
 import Post from './Components/Post'
 import './App.css';
 import * as settings from './settings'
-
+import { GROUP_ID, ACCESS_TOKEN } from './settings';
+var format = require('string-format')
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+    }
+    this.base_url = 'https://graph.facebook.com/v2.11';
+    this.paging = ''
+    this.node = format('/{}/feed', GROUP_ID)
+    this.params = format('/?limit={}&access_token={}', 10, ACCESS_TOKEN)
+    // Needs more?
+  }
+
 
   componentDidMount() {
 
@@ -16,6 +29,26 @@ class App extends Component {
       .catch(function(error) {
         console.log(error);
       });
+    console.log(format('Hello, {}', 'World'));
+  }
+
+  constructFBPageFieldUrl = (baseUrl) => {
+    const fields = '&fields=link,updated_time,type,picture,full_picture, \
+    message,shares,reactions.limit(0).summary(true)';
+    const url = baseUrl + fields
+    return url
+  }
+
+  scrapeFBPageFeedPosts = (groupID, accessToken) => {
+
+  }
+
+  processFBPageFeedPosts = (post) => {
+
+  }
+
+  fetchData = (url) => {
+
   }
 
   render() {
