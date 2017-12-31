7890bca108ed369333f2604b2f65067b19632650
Jimmy Chen <jzc8@cornell.edu>
2017-12-30 23:36:18 -0500

    Add axios and implement placeholder function for fetching data

--- ez-dogspotting/src/App.js
+++ ez-dogspotting/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import Post from './Components/Post'
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(function(response) {
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
