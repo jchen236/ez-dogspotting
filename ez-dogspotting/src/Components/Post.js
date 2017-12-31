c0bb4f070c397a2edebf8c7400a073995c316258
Jimmy Chen <jzc8@cornell.edu>
2017-12-30 23:17:25 -0500

    Post component outline

---
+++ ez-dogspotting/src/Components/Post.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Post extends Component {
+
+  render() {
+    return (
+      <h1>This is a post </h1>
+    ); 
+  }
+}
+
+
+export default Post;
