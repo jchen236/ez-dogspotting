3d10a3e03f1233b833e6709a84b95f56684ee315
Jimmy Chen <jzc8@cornell.edu>
2018-01-02 23:27:07 -0500

    Pass props to state in PostItem.js

--- ez-dogspotting/src/Components/PostItem.js
+++ ez-dogspotting/src/Components/PostItem.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class PostItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.postData.id,
+            link: props.postData.link,
+            type: props.postData.type,
+            full_picture: props.postData.full_picture,
+            picture: props.postData.picture,
+            message: props.postData.message,
+            reactions: props.postData.reactions,
+            shares: props.postData.shares,
+            updated_time: props.postData.updated_time      
+        };
+        // Needs more?
+      }
     
   render() {
-    let postData;
-    if (this.props.postData) {
-        console.log("Props received from Posts to PostItem");
-        console.log(this.props.postData);
-    }
     return (
       <li>
-        This is a post item
+        {this.state.link}
       </li>
     ); 
   }
