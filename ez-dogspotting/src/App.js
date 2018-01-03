e557de111593f75b6d32f0dc843374f708713f9c
Jimmy Chen <jzc8@cornell.edu>
2018-01-02 23:39:44 -0500

    Embed fb video into site

--- ez-dogspotting/src/App.js
+++ ez-dogspotting/src/App.js
@@ -82,6 +82,15 @@ class App extends Component {
     return (
       <div className="App">
         My App
+        {
+          ((d, s, id) => {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+            fjs.parentNode.insertBefore(js, fjs);
+          })(document, 'script', 'facebook-jssdk')
+        }
         <Posts posts = {this.state.data}/>
       </div>
     );
