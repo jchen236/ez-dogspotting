f4e0162509261a56d56a44368fa90b49234a4738
Jimmy Chen <jzc8@cornell.edu>
2018-01-03 22:07:47 -0500

    Render embedded video for videos

--- ez-dogspotting/src/Components/PostItem.js
+++ ez-dogspotting/src/Components/PostItem.js
@@ -22,17 +22,12 @@ class PostItem extends Component {
   render() {
     return (
         <div>
-      <li>
+      {/* <li>
         {this.state.link}
-      </li>
+      </li> */}
 
-      <div class="fb-video" data-href="https://www.facebook.com/facebook/videos/10153231379946729/" data-width="500" data-show-text="false">
+      <div class="fb-video" data-href={this.state.link} data-height= "400" data-width="300" data-show-text="false">
     <div class="fb-xfbml-parse-ignore">
-      <blockquote cite="https://www.facebook.com/facebook/videos/10153231379946729/">
-        <a href="https://www.facebook.com/facebook/videos/10153231379946729/">How to Share With Just Friends</a>
-        <p>How to share with just friends.</p>
-        Posted by <a href="https://www.facebook.com/facebook/">Facebook</a> on Friday, December 5, 2014
-      </blockquote>
     </div>
     </div>
       </div>
