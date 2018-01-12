e4df9023ee0d104d4ad4207f7b31a142fe7501ef
Jimmy Chen <jzc8@cornell.edu>
2018-01-12 01:28:39 -0500

    Successfully get source from axios request

--- ez-dogspotting/src/Components/PostComponents/MediaContainer.js
+++ ez-dogspotting/src/Components/PostComponents/MediaContainer.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import { GROUP_ID, ACCESS_TOKEN } from '../../settings';
+import axios from 'axios';
+var format = require('string-format');
 
 class Media extends Component {
 
+    componentDidMount() {
+        let ids = this.extractTargetIDFromAttachments();
+        const base_url = 'https://graph.facebook.com/v2.11';
+        const node = format('/{}', ids[0]);
+        const params = format('/?limit={}&access_token={}', 10, ACCESS_TOKEN);
+        const fields = '&fields=source';
+        const url = base_url + node + params + fields;
+        console.log(url)
+        axios.get(url)
+        .then((response) => {
+            console.log(response.data);
 
+        })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log("Media componentDidMount finished calling");
+
+    }
     extractTargetIDFromAttachments = () => {
         if(this.props.attachmentData) {
             let attachmentData = this.props.attachmentData.data[0];
@@ -20,6 +40,10 @@ class Media extends Component {
                 return [attachmentData.target.id]
             }
         }
+    }
+
+    get_source_from_id = (id) => {
+
     }
 
   render() {
