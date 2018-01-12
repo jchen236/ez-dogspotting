991325c4c47a39e9d16c632df1579633d906c8d1
Jimmy Chen <jzc8@cornell.edu>
2018-01-12 00:42:31 -0500

    Remove state usage from media component and only use props in render function

--- ez-dogspotting/src/Components/PostComponents/Media.js
+++ ez-dogspotting/src/Components/PostComponents/Media.js
@@ -4,44 +4,35 @@ import PropTypes from 'prop-types';
 
 class Media extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            attachments: props.attachmentData.data[0],
-            content_ids: []
-        };
-        this.extractTargetIDFromAttachments();
-      }
 
     extractTargetIDFromAttachments = () => {
-        if(this.state.attachments.subattachments) {
-            console.log("This is a multi-part post");
-            const content_ids = this.state.attachments.subattachments.data.map(subattachment => {
-                return subattachment.target.id;
-            });
-            console.log(content_ids)
-            this.setState({
-                content_ids: content_ids
-            });
-        } else {
-            console.log("this is a single-part post");
-            console.log(this.state.attachments.target.id)
-            this.setState({
-                content_ids: [this.state.attachments.target.id]
-            });
+        if(this.props.attachmentData) {
+            let attachmentData = this.props.attachmentData.data[0];
+            if(attachmentData.subattachments) {
+                console.log("This is a multi-part post");
+                let content_ids = attachmentData.subattachments.data.map(subattachment => {
+                    return subattachment.target.id;
+                });
+                // console.log(content_ids)
+                return content_ids;
+            } else {
+                console.log("this is a single-part post");
+                return [attachmentData.target.id]
+            }
         }
     }
 
   render() {
+    let content_ids = this.extractTargetIDFromAttachments();
     var divStyle = {
         color: 'white',
         backgroundColor: 'red'
       };
-      console.log(this.state.content_ids);
+      console.log(content_ids)
     return (
       <div className="Media">
       <h1 style ={divStyle} >
-        {this.state.content_ids}
+        {content_ids}
         </h1>
       </div>
     ); 
