b1511a4480dda2916fe1d55941de250e4d53f745
Jimmy Chen <jzc8@cornell.edu>
2018-01-11 23:10:57 -0500

    Add logic to extract ids from post

---
+++ ez-dogspotting/src/Components/PostComponents/Media.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Media extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            attachments: props.data[0],
+            content_ids: []
+        };
+      }
+
+    extractTargetIDFromAttachments = () => {
+        let id_list = [];
+        if(this.state.subattachments) {
+            console.log("This is a multi-part post");
+            let subattachments = this.state.subattachments.data;
+            this.setState({
+                content_ids: subattachments.map(subattachment => {
+                    subattachment.target.id;
+                })
+            });
+        } else {
+            console.log("this is a single-part post");
+            this.setState({
+                content_ids: [this.state.attachments.target.id]
+            });
+        }
+    }
+      
+  render() {
+    return (
+      <div className="Media">
+        MediaComponent
+      </div>
+    ); 
+  }
+}
+
+
+export default Media;
