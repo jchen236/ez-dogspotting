87dc8bb0cb5c001ce501eb7c7abdbde1d6db61ef
Jimmy Chen <jzc8@cornell.edu>
2018-01-12 01:43:37 -0500

    Trying to get axios to return non null value

--- ez-dogspotting/src/Components/PostComponents/MediaContainer.js
+++ ez-dogspotting/src/Components/PostComponents/MediaContainer.js
@@ -6,25 +6,25 @@ var format = require('string-format');
 
 class Media extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+          sources: []
+        };
+        // Needs more?
+      }
+
     componentDidMount() {
         let ids = this.extractTargetIDFromAttachments();
-        const base_url = 'https://graph.facebook.com/v2.11';
-        const node = format('/{}', ids[0]);
-        const params = format('/?limit={}&access_token={}', 10, ACCESS_TOKEN);
-        const fields = '&fields=source';
-        const url = base_url + node + params + fields;
-        console.log(url)
-        axios.get(url)
-        .then((response) => {
-            console.log(response.data);
-
-        })
-      .catch((error) => {
-        console.log(error);
-      });
-    console.log("Media componentDidMount finished calling");
-
+        let sources = ids.map(id => {
+            let foo = this.get_source_from_id(id);
+            console.log(foo)
+            console.log("foo")
+            return foo;
+        } );
+        this.setState({ sources:sources });
     }
+
     extractTargetIDFromAttachments = () => {
         if(this.props.attachmentData) {
             let attachmentData = this.props.attachmentData.data[0];
@@ -43,20 +43,34 @@ class Media extends Component {
     }
 
     get_source_from_id = (id) => {
-
+        const base_url = 'https://graph.facebook.com/v2.11';
+        const node = format('/{}', id);
+        const params = format('/?limit={}&access_token={}', 10, ACCESS_TOKEN);
+        const fields = '&fields=source';
+        const url = base_url + node + params + fields;
+        console.log(url)
+        axios.get(url)
+            .then((response) => {
+                console.log("done")
+                console.log(response.data);
+                return response.data.source;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        console.log("Media componentDidMount finished calling");
     }
 
   render() {
-    let content_ids = this.extractTargetIDFromAttachments();
     var divStyle = {
         color: 'white',
         backgroundColor: 'red'
       };
-      console.log(content_ids)
+      console.log(this.state.sources);
     return (
       <div className="Media">
       <h1 style ={divStyle} >
-        {content_ids}
+        {this.state.sources}
         </h1>
       </div>
     ); 
