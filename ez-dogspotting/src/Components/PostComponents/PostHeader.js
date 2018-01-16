2ad67e9b4803a021f70a0656a049c39f8fbc58f9
Jimmy Chen <jzc8@cornell.edu>
2018-01-15 23:42:42 -0500

    Add PostHeader Component

---
+++ ez-dogspotting/src/Components/PostComponents/PostHeader.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../../Assets/css/default.min.css';
+var format = require('string-format');
+
+class PostHeader extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+      }
+
+   
+  render() {
+ 
+    return (
+      <div className = 'post_header'>
+      </div>
+    ); 
+  }
+}
+
+
+export default PostHeader;
