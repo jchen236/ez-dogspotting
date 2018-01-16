cd87c18acc96af57f72056ad5fed555a6b178af9
Jimmy Chen <jzc8@cornell.edu>
2018-01-15 23:43:22 -0500

    Add PostMessage Component

---
+++ ez-dogspotting/src/Components/PostComponents/PostMessage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../../Assets/css/default.min.css';
+var format = require('string-format');
+
+class PostMessage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+      }
+
+   
+  render() {
+ 
+    return (
+      <div className = 'post_message'>
+      </div>
+    ); 
+  }
+}
+
+
+export default PostMessage;
